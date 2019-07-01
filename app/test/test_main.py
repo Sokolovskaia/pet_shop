454a6601a25ad79789ede38294005387d716f44e
Sokolovskaia <sk2m.sokolovskaya@google.com>
2019-07-02 00:25:00 +0300

    test preparation

--- app/test/test_main.py
+++ app/test/test_main.py
@@ -5,6 +5,7 @@ import waitress
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 
 from app.test import test_db
+from app import main
 
 import os
 
@@ -20,6 +21,25 @@ ADDS_PER_PAGE = 3
 
 
 
+app = Flask(__name__)
+app.config['uploads'] = UPLOAD_FOLDER
 
-def test_test():
+app.config.from_mapping(
+    SECRET_KEY='Marina_secret_key_test',
+)
+db_url = 'test_db.sqlite'
+
+
+def user():
+    if 'id' in session and session['id'] is not None:
+        user_login = session['login']
+    else:
+        user_login = 0
+    return user_login
+
+def test_search():
     pass
+
+
+
+

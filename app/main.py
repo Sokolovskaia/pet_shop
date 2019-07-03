c2996020027cfbc98ae90ae2cabc4841d86b6ddb
Sokolovskaia <sk2m.sokolovskaya@google.com>
2019-07-04 01:28:22 +0300

    adding content

--- app/main.py
+++ app/main.py
@@ -317,7 +317,7 @@ def start():
     if os.getenv('APP_ENV') == 'PROD' and os.getenv('PORT'):
         waitress.serve(app, port=os.getenv('PORT'))
     else:
-        app.run(port=9875, debug=True)
+        app.run(port=9871, debug=True)
 
 
 if __name__ == '__main__':

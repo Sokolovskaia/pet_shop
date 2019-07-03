cfab810da50bc6ebf5fff3a98a0d6021cef460df
Sokolovskaia <sk2m.sokolovskaya@google.com>
2019-07-04 01:02:00 +0300

    adding content

--- app/test/test_main.py
+++ app/test/test_main.py
@@ -27,30 +27,29 @@ app.config.from_mapping(
 )
 db_url = 'test_db.sqlite'
 
-#
-# def open_db(url):
-#     connection = sqlite3.connect(url)
-#     connection.row_factory = sqlite3.Row
-#     return connection
-#
-#
-# def user():
-#     if 'id' in session and session['id'] is not None:
-#         user_login = session['login']
-#     else:
-#         user_login = 0
-#     return user_login
-#
-#
-# def test_all_pets():
-#     expected = ({'ad_id': 1, 'category': 'Собака', 'category_lowercased': 'собака', 'breed': 'овчарка',
-#                  'breed_lowercased': 'овчарка', 'gender': 'М', 'birtdate': '01.2000', 'name': 'Жук',
-#                  'name_lowercased': 'жук', 'price': 1, 'photo': None, 'description': 'Умный', 'author_id': 1})
-#     actual_1 = db.all_pets(open_db(db_url), ADDS_PER_PAGE, pages_offset=0)
-#     actual = actual_1[0]
-#     assert expected == actual
-#
+
+def open_db(url):
+    connection = sqlite3.connect(url)
+    connection.row_factory = sqlite3.Row
+    return connection
+
+
+def user():
+    if 'id' in session and session['id'] is not None:
+        user_login = session['login']
+    else:
+        user_login = 0
+    return user_login
+
+
+def test_all_pets():
+    expected = ({'ad_id': 1, 'category': 'Собака', 'category_lowercased': 'собака', 'breed': 'Овчарка',
+                 'breed_lowercased': 'овчарка', 'gender': 'М', 'birthdate': '01.2000', 'name': 'Жук',
+                 'name_lowercased': 'жук', 'price': 1, 'photo': None, 'description': 'Умный', 'author_id': 1})
+    actual_1 = db.all_pets(open_db(db_url), ADDS_PER_PAGE, pages_offset=0)
+    actual = dict(actual_1[0])
+    assert expected == actual
+
 
 def test_test():
     pass
-
